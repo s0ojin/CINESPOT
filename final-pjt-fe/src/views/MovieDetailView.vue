4f4b958b7fe0131af9808e5c752f30002ae75e64
s0ojin <onepst@hanyang.ac.kr>
2024-05-16 15:51:47 +0900

    setting: router 설정

--- final-pjt-fe/src/views/MovieDetailView.vue
+++ final-pjt-fe/src/views/MovieDetailView.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
   <div>
-    <h1 class="text-lg">Home</h1>
+    <h1 class="text-lg">MovieDetailView</h1>
   </div>
 </template>
---
+++ final-pjt-fe/src/views/SignUpView.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>
+    <h1 class="text-lg">SignUpView</h1>
+  </div>
+</template>
