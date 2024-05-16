d7f635e7a44dc4384a2226a242e7d5ae7e886bb3
박수진 <one_pst@naver.com>
2024-05-16 15:51:47 +0900

    setting: router 설정

--- final-pjt-fe/src/router/index.js
+++ final-pjt-fe/src/router/index.js
@@ -1,14 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MovieListView from '@/views/MovieListView.vue'
+import MovieDetailView from '@/views/MovieDetailView.vue'
+import SignUpView from '@/views/SignUpView.vue'
+import SignInView from '@/views/SignInView.vue'
+import MypageView from '@/views/MypageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/movies',
+      name: 'movies',
+      component: MovieListView
     },
+    {
+      path: '/movies/:movieId',
+      name: 'movieDetail',
+      component: MovieDetailView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignInView
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: MypageView
+    }
   ]
 })
 
