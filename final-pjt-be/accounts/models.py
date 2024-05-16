8e53a7664a5b3c3a630703736df22a89e64010ba
PKR <fbxm12@naver.com>
2024-05-16 17:39:01 +0900

    회원가입(django), 영화 모델설계(REST)

--- final-pjt-be/accounts/models.py
+++ final-pjt-be/accounts/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 # 회원 모델 구현
+class User(AbstractUser):
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    # pass # 팔로워 기능 구현 안할 때
--- final-pjt-be/movies/models.py
+++ final-pjt-be/movies/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 # 영화 배우, 감독, 게시글 모델 구현
+
+class Movie(models.Model):
+    title = models.CharField(max_length = 150)
+    actors = models.CharField(max_length = 150)
+    director = models.CharField(max_length = 150)
+    overview = models.TextField()
+    relese_data = models.DateTimeField()
+    poster_path = models.TextField() 
+
+class Actor(models.Model):
+    name =  models.CharField(max_length = 150)
+'''
+class Director(models.Model):
+    pass
+'''
+class Comment(models.Model):
+    content = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+class Movie_like_users(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
