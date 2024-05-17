3c408c100b29c7f38e767547146d0d225d5f5054
s0ojin <onepst@hanyang.ac.kr>
2024-05-18 00:53:38 +0900

    feat: 로그인 UI작업

--- final-pjt-fe/tailwind.config.js
+++ final-pjt-fe/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary-50': '#E7EDFF',
+        'primary-100': '#BDCCFF',
+        'primary-200': '#93ABFF',
+        'primary-300': '#698AFF',
+        'primary-400': '#3F6AFF',
+        'primary-500': '#1447F8',
+        'primary-600': '#012FCF',
+        'primary-700': '#0025A6',
+        'primary-800': '#001C7E',
+        'primary-900': '#001355'
+      }
+    }
   },
   plugins: []
 }
