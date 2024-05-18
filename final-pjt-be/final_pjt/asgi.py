90030c70596a94fa450f8df61c4fd1d927e088ae
Kyoungjaa <fbxm12@naver.com>
2024-05-18 15:08:08 +0900

    환경이슈로 뒤엎고 재생성

--- final-pjt-be/final_pjt/asgi.py
+++ final-pjt-be/final_pjt/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for finalpjt project.
+ASGI config for final_pjt project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalpjt.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final_pjt.settings")
 
 application = get_asgi_application()
