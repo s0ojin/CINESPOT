d9fe92e8c0cbd7c49333130a72234889d69b08ee
박수진 <one_pst@naver.com>
2024-05-24 04:54:07 +0900

    feat: 여행지 카드 UI 추가

---
+++ final-pjt-fe/src/components/TravelCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="max-w-sm rounded-lg overflow-hidden shadow-lg my-4">
+    <img :src="image_url" alt="Destination image" class="w-full" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ destination }}</div>
+      <p class="text-gray-700 text-base">
+        {{ reason }}
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{
+        address
+      }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  destination: String,
+  reason: String,
+  address: String,
+  image_url: String
+})
+</script>
