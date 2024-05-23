f3710fb7a30781badeb9eea8d441988645ea4df5
Kyoungjaa <fbxm12@naver.com>
2024-05-23 11:28:44 +0900

    HERE IS userInfo WHAT U WANT

--- final-pjt-be/accounts/urls.py
+++ final-pjt-be/accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 appname = 'accounts'
 urlpatterns = [
-   path('user_info/<int:user_id>/', views.user_info), # 유저 정보여
+   path('user_info/', views.user_info), # 유저 정보여
    path('my_page/', views.my_page), # 마이페이지용
 ]
